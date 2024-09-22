b56be624879587a77be436cbedfd3e32504aef1e
Tydron16 <114885071+ClaReQurv@users.noreply.github.com>
2024-09-22 11:24:22 +0200

    update

---
+++ build/index.js
@@ -0,0 +1,6 @@
+export default class ReQurvUtils {
+    constructor() { }
+    checkClass() {
+        return "ReQurvUtils";
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export default class ReQurvUtils {
+  constructor() {}
+
+  public checkClass() {
+    return "ReQurvUtils";
+  }
+}
